6854ba6a0da0e853b42dde2c5aae1e6a33862ebf
msg2324 <igiza1213@gmail.com>
2024-11-06 15:10:55 +0900

    feature : Signin and Signup UI

--- app/src/main/java/com/example/android_foryou/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/android_foryou/screens/home/HomeScreen.kt
@@ -1,24 +1,32 @@
 package com.example.android_foryou.screens.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.android_foryou.R
 
 @Composable
 fun HomeScreen(navController: NavController) {
@@ -29,26 +37,45 @@ fun HomeScreen(navController: NavController) {
     ) {
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(top = 96.dp),
-            verticalArrangement = Arrangement.spacedBy(35.dp, Alignment.CenterVertically),
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(35.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Row(
                 modifier = Modifier
                     .shadow(
-                        elevation = 4.dp,
-                        spotColor = Color(0x14000000),
-                        ambientColor = Color(0x14000000)
+                        elevation = 10.dp,
+                        spotColor = Color(0xAA000000),
+                        ambientColor = Color(0xAA000000)
                     )
-                    .width(375.dp)
-                    .height(153.dp)
+                    .fillMaxWidth()
+                    .height(100.dp)
                     .background(color = Color(0xFFFFFFFF), shape = RoundedCornerShape(size = 25.dp))
-                    .padding(start = 15.dp, top = 33.dp, end = 15.dp, bottom = 33.dp),
+                    .padding(),
                 horizontalArrangement = Arrangement.spacedBy(241.dp, Alignment.CenterHorizontally),
-                verticalAlignment = Alignment.Bottom,
+                verticalAlignment = Alignment.CenterVertically,
             ) {
-                // Child views.
+                Row {
+                    Image(
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = "logo",
+                        modifier = Modifier
+                            .width(37.dp)
+                            .height(34.dp)
+                    )
+                    Text(
+                        text = "회원가입",
+                        fontSize = 18.sp,
+                        lineHeight = 21.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = Color(0xFFEA8895),
+                    )
+
+                    Image(
+                        painter = painterResource(id = R.drawable.vector),
+                        contentDescription = "image description",
+                    )
+                }
             }
         }
     }
