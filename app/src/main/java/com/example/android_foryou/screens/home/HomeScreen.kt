909fc8f0ccd927e7c85a41369d428d3f6aa2f0f7
msg2324 <igiza1213@gmail.com>
2024-11-12 20:56:21 +0900

    feature home ui

--- app/src/main/java/com/example/android_foryou/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/android_foryou/screens/home/HomeScreen.kt
@@ -1,23 +1,34 @@
 package com.example.android_foryou.screens.home
 
+import android.widget.HorizontalScrollView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -38,44 +49,195 @@ fun HomeScreen(navController: NavController) {
         Column(
             modifier = Modifier
                 .fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(35.dp),
+            verticalArrangement = Arrangement.spacedBy(22.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Row(
-                modifier = Modifier
-                    .shadow(
-                        elevation = 10.dp,
-                        spotColor = Color(0xAA000000),
-                        ambientColor = Color(0xAA000000)
-                    )
-                    .fillMaxWidth()
-                    .height(100.dp)
-                    .background(color = Color(0xFFFFFFFF), shape = RoundedCornerShape(size = 25.dp))
-                    .padding(),
-                horizontalArrangement = Arrangement.spacedBy(241.dp, Alignment.CenterHorizontally),
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Row {
-                    Image(
-                        painter = painterResource(id = R.drawable.logo),
-                        contentDescription = "logo",
-                        modifier = Modifier
-                            .width(37.dp)
-                            .height(34.dp)
-                    )
-                    Text(
-                        text = "회원가입",
-                        fontSize = 18.sp,
-                        lineHeight = 21.sp,
-                        fontWeight = FontWeight.SemiBold,
-                        color = Color(0xFFEA8895),
-                    )
 
-                    Image(
-                        painter = painterResource(id = R.drawable.vector),
-                        contentDescription = "image description",
-                    )
+            TopCard()
+            BottomCard()
+        }
+    }
+}
+
+@Composable
+fun TopCard() {
+    Row(
+        modifier = Modifier
+            .shadow(
+                elevation = 10.dp,
+                spotColor = Color(0xAA000000),
+                ambientColor = Color(0xAA000000)
+            )
+            .fillMaxWidth()
+            .height(120.dp)
+            .background(color = Color(0xFFFFFFFF), shape = RoundedCornerShape(size = 25.dp))
+            .padding(),
+        horizontalArrangement = Arrangement.spacedBy(241.dp, Alignment.CenterHorizontally),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "logo",
+                modifier = Modifier
+                    .width(37.dp)
+                    .height(34.dp)
+            )
+            Text(
+                text = "회원가입",
+                fontSize = 18.sp,
+                lineHeight = 21.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = Color(0xFFEA8895),
+            )
+
+
+        }
+
+        Image(
+            painter = painterResource(id = R.drawable.vector),
+            contentDescription = "image description",
+            modifier = Modifier
+                .padding(2.dp)
+                .width(14.dp)
+                .height(18.dp)
+        )
+    }
+}
+
+@Composable
+fun BottomCard() {
+    Column(
+        modifier = Modifier
+            .shadow(
+                elevation = 10.dp,
+                spotColor = Color(0xAA000000),
+                ambientColor = Color(0xAA000000)
+            )
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(color = Color(0xFFFFFFFF), shape = RoundedCornerShape(size = 25.dp))
+            .padding(horizontal = 23.dp, vertical = 14.dp),
+
+        ) {
+        Text(
+            text = "선호도 Top 5",
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(Modifier.height(19.dp))
+
+        Row(modifier = Modifier.horizontalScroll(rememberScrollState())) {
+            HomeData.hotItems.forEachIndexed { _, hotModel ->
+                Column(
+                    modifier = Modifier
+                        .shadow(
+                            elevation = 4.dp,
+                            spotColor = Color(0x40000000),
+                            ambientColor = Color(0x40000000)
+                        )
+                        .width(167.dp)
+                        .height(209.dp)
+                        .paint(
+                            painter = painterResource(id = hotModel.image),
+                            contentScale = ContentScale.FillBounds
+                        )
+                        .padding(bottom = 16.dp, start = 12.dp),
+                    verticalArrangement = Arrangement.Bottom
+                ) {
+                    Column {
+                        Text(
+                            text = hotModel.title,
+                            fontWeight = FontWeight.Bold,
+                            color = Color(0xFFFFFFFF),
+                            fontSize = 25.sp
+                        )
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Image(
+                                painter = painterResource(id = R.drawable.heart),
+                                contentDescription = "heart",
+                                modifier = Modifier
+                                    .width(14.dp)
+                                    .height(14.dp)
+                            )
+
+                            Spacer(Modifier.width(5.dp))
+
+                            Text(
+                                text = (hotModel.good / 1000.0).toString() + "K",
+                                fontWeight = FontWeight.Normal,
+                                color = Color(0xFFFFFFFF),
+                                fontSize = 12.sp
+                            )
+                        }
+                    }
                 }
+
+                Spacer(modifier = Modifier.width(10.dp))
+            }
+        }
+
+        Spacer(Modifier.height(38.dp))
+
+        Column {
+            HomeData.listItems.forEachIndexed { _, listModel ->
+                ListCard(listModel)
+                Divider(thickness = 2.dp, modifier = Modifier.padding(vertical = 15.dp))
+            }
+        }
+    }
+
+}
+
+@Composable
+fun ListCard(listModel: ListModel) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.Start),
+        verticalAlignment = Alignment.Top,
+    ) {
+        Image(
+            painter = painterResource(id = listModel.image),
+            contentDescription = "image",
+            modifier = Modifier
+                .width(80.dp)
+                .height(80.dp)
+        )
+
+
+        Column {
+            Text(
+                text = listModel.title,
+                fontWeight = FontWeight.Bold,
+                fontSize = 14.sp
+            )
+
+            Spacer(Modifier.height(6.dp))
+
+            Text(
+                text = listModel.description,
+                fontWeight = FontWeight.Normal,
+                fontSize = 10.sp,
+                color = Color(0xFF959595)
+            )
+
+            Spacer(Modifier.height(19.dp))
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.drawable.heartpink),
+                    contentDescription = "heart",
+                    modifier = Modifier
+                        .width(14.dp)
+                        .height(14.dp),
+                )
+
+                Spacer(Modifier.width(3.dp))
+
+                Text(
+                    text = (listModel.good).toString(),
+                    fontWeight = FontWeight.Normal,
+                    color = Color(0xFFEA8895),
+                    fontSize = 12.sp
+                )
             }
         }
     }
--- app/src/main/java/com/example/android_foryou/screens/signupDetail/SignupDetailScreen.kt
+++ app/src/main/java/com/example/android_foryou/screens/signupDetail/SignupDetailScreen.kt
@@ -3,22 +3,27 @@ package com.example.android_foryou.screens.home
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Divider
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TriStateCheckbox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,6 +40,14 @@ fun SignupDetailScreen(navController: NavController) {
     var gender by remember { mutableStateOf("") }
     var age by remember { mutableStateOf("") }
     var birth by remember { mutableStateOf("") }
+
+    val childCheckedStates = remember { mutableStateListOf(false, false) }
+
+    val parentState = when {
+        childCheckedStates.all { it } -> ToggleableState.On
+        childCheckedStates.none { it } -> ToggleableState.Off
+        else -> ToggleableState.Indeterminate
+    }
 
     Surface(
         modifier = Modifier.fillMaxSize()
@@ -89,6 +102,41 @@ fun SignupDetailScreen(navController: NavController) {
                 label = { Text(text = "생일") })
 
             Divider(thickness = 2.dp, modifier = Modifier.padding(top = 15.dp))
+
+            Column(
+                verticalArrangement = Arrangement.spacedBy(29.dp, Alignment.Top),
+                horizontalAlignment = Alignment.Start,
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text("Select all")
+                    TriStateCheckbox(
+                        state = parentState,
+                        onClick = {
+                            // Determine new state based on current state
+                            val newState = parentState != ToggleableState.On
+                            childCheckedStates.forEachIndexed { index, _ ->
+                                childCheckedStates[index] = newState
+                            }
+                        }
+                    )
+                }
+                childCheckedStates.forEachIndexed { index, checked ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Text(if (index == 0) "[필수] 이용약관 동의" else "[필수] 개인정보 수집 및 이용 동의")
+                        Checkbox(
+                            checked = checked,
+                            onCheckedChange = { isChecked ->
+                                // Update the individual child state
+                                childCheckedStates[index] = isChecked
+                            }
+                        )
+                    }
+                }
+            }
 
             AuthButton(text = "Next", onClick = {
                 navController.navigate(Screens.Home.title)
